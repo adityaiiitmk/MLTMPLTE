9228f566892986e5b8477fb0ab631622d2fd78eb
adityaiiitmk <aditya.s@duk.ac.in>
2023-09-19 15:18:13 +0530

    Rename

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "MLTMPLTE"
+project_name = "projName"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
